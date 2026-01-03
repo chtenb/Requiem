daeaf5e188276dabc3ed088fe093eec3ca66e9fa
chtenb <ctenbrinke@gmail.com>
2026-01-03 19:49:11 +0100

    add test project

---
+++ Requiem.Test/Test.cs
@@ -0,0 +1,12 @@
+using CsCheck;
+
+namespace Requiem.Test;
+
+public class Test
+{
+    [Test]
+    public void TestParameterizedGenerators()
+    {
+        // TODO: call generators with randomized parameters to make sure they don't throw
+    }
+}
